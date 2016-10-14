5625d0fa7fe3194a1990b962eb7ac6d1ae6719a4
Evgeniy <evgeniy.fm@gmail.com>
2016-10-14 14:26:11 +0300

    travis

--- dlib/travis/before-install.sh
+++ dlib/travis/before-install.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Exit if anything fails.
+set -eux
+
+HERE=$PW
+
+
+mkdir -v $HOME/bin
+for g in gcc g++ gcov gcc-ar gcc-nm gcc-ranlib
+do
+  test -x $( type -p ${g}-$GCC_VER )
+  ln -sv $(type -p ${g}-$GCC_VER) $HOME/bin/${g}
+done
 
 if [[ ! -x cmake/bin/cmake && -d cmake ]]; then
     rm -fr cmake
@@ -8,4 +20,8 @@ if [[ ! -d cmake && ${BUILD_SYSTEM:-} == cmake ]]; then
   CMAKE_URL="http://www.cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz"
   mkdir cmake && wget --no-check-certificate -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
 fi
+
+
+# NOTE, changed from PWD -> HOME
+export PATH=$HOME/bin:$PATH
 
