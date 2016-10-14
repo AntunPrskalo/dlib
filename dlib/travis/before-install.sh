5a78d6ea4cd0c53e0186b3b3d622921efcdc9618
Evgeniy <evgeniy.fm@gmail.com>
2016-10-14 15:08:24 +0300

    travis

--- dlib/travis/before-install.sh
+++ dlib/travis/before-install.sh
@@ -13,12 +13,12 @@ do
   ln -sv $(type -p ${g}-$GCC_VER) $HOME/bin/${g}
 done
 
-if [[ ! -x cmake/bin/cmake && -d cmake ]]; then
-    rm -fr cmake
-fi
-if [[ ! -d $PWD/cmake && ${BUILD_SYSTEM:-} == cmake ]]; then
-  CMAKE_URL="http://www.cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz"
-  mkdir -v cmake 
-  wget --no-check-certificate -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-fi
+#if [[ ! -x cmake/bin/cmake && -d cmake ]]; then
+#    rm -fr cmake
+#fi
+#if [[ ! -d $PWD/cmake && ${BUILD_SYSTEM:-} == cmake ]]; then
+#  CMAKE_URL="http://www.cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz"
+#  mkdir -v cmake 
+#  wget --no-check-certificate -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
+#fi
 
